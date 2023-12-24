380749f2c255162debf18af12de4ba3e52e84380
root <root@DESKTOP-VS3635G>
2023-12-24 20:48:04 +1100

    Possibly fixed origami in nvim

--- nvim/lua/user/plugins/nvim-origami.lua
+++ nvim/lua/user/plugins/nvim-origami.lua
@@ -1,5 +1,6 @@
 return {
   "chrisgrieser/nvim-origami",
+  enabled = true,
   event = "BufReadPost", -- later or on keypress would prevent saving folds
   opts = true, -- needed even when using default config
 }
