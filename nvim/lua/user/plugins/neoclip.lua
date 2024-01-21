00310fa1fd6f15d551542423289d448f3ace0afc
root <root@DESKTOP-VS3635G>
2024-01-21 20:38:43 +1100

    Added multline edit plugin Reduced history for neoclip to 100

--- nvim/lua/user/plugins/neoclip.lua
+++ nvim/lua/user/plugins/neoclip.lua
@@ -5,10 +5,10 @@ return {
     -- you'll need at least one of these
     { "nvim-telescope/telescope.nvim" },
     { "kkharji/sqlite.lua",           module = "sqlite" },
-    -- {'ibhagwan/fzf-lua'},
   },
   config = function()
     require("neoclip").setup {
+      history = 100,
       enable_persistent_history = true,
       continuous_sync = true,
     }
