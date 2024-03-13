d5979983e03302daa6ee3c6cdd9575e42b6dc445
root <root@DESKTOP-VS3635G>
2024-03-13 17:14:15 +1100

    Added key for normal mode in terminal

--- nvim/lua/user/plugins/toggleterm.lua
+++ nvim/lua/user/plugins/toggleterm.lua
@@ -10,7 +10,7 @@ return {
     -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
     -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
     config.open_mapping = [[<c-t>]]
-    config.direction = "float";
+    config.direction = "float"
     config.sources = {
       -- Set a formatter
       -- null_ls.builtins.formatting.stylua,
@@ -18,11 +18,14 @@ return {
     }
     config.shell = function()
       if string.find(vim.loop.os_uname().sysname, "Win") then
-        return "powershell.exe";
+        return "powershell.exe"
       else
         return vim.o.shell
       end
-    end;
+    end
+
+    -- Toggle normal mode
+    vim.api.nvim_set_keymap("t", "<Esc>", "<C-\\><C-n>", { noremap = true, silent = true })
     return config -- return final config table
   end,
 }
