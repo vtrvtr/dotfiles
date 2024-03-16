1a2cbc0f0caa94ebe5f7b5508bcae42bea28faa2
root <root@DESKTOP-VS3635G>
2024-03-16 11:32:28 +1100

    Added tint plugin to dim inactive splits

---
+++ nvim/lua/user/plugins/tint.lua
@@ -0,0 +1,8 @@
+return {
+  "levouh/tint.nvim",
+  event = "BufEnter",
+  opts = {
+    tint = -65,       -- Darken colors, use a positive value to brighten
+    saturation = 0.8, -- Saturation to preserve
+  },
+}
