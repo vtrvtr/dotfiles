b003eb83635e11a9031950341298bdb1351f174f
root <root@DESKTOP-VS3635G>
2024-03-19 23:04:27 +1100

    Added Muren for search and replace

---
+++ nvim/lua/user/plugins/muren.lua
@@ -0,0 +1,11 @@
+return {
+  "AckslD/muren.nvim",
+  cmd = "MurenToggle",
+  opts = {
+    -- ui sizes
+    patterns_width = 50,
+    patterns_height = 15,
+    options_width = 40,
+    preview_height = 30,
+  }
+}
