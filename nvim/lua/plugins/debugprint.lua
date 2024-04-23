ea5c8ca46a305fd299224613088e7bf17ec5cd55
root <root@DESKTOP-VS3635G>
2024-04-23 18:10:55 +1000

    Added debugprint plugin

---
+++ nvim/lua/plugins/debugprint.lua
@@ -0,0 +1,49 @@
+return {
+  "andrewferrier/debugprint.nvim",
+  config = function()
+    local opts = {
+      keymaps = {
+        normal = {
+          plain_below = "<leader>ddp",
+          plain_above = "<leader>ddP",
+          variable_below = "<leader>ddv",
+          variable_above = "<leader>ddV",
+          variable_below_alwaysprompt = nil,
+          variable_above_alwaysprompt = nil,
+          textobj_below = "<leader>ddo",
+          textobj_above = "<leader>ddO",
+          toggle_comment_debug_prints = nil,
+          delete_debug_prints = nil,
+        },
+        visual = {
+          variable_below = "<leader>ddv",
+          variable_above = "<leader>ddV",
+        },
+      },
+      commands = {
+        toggle_comment_debug_prints = "ToggleCommentDebugPrints",
+        delete_debug_prints = "DeleteDebugPrints",
+      },
+    }
+    local wk = require "which-key"
+    wk.register({
+      d = {
+        d = {
+          name = "+Debug print",
+          v = {
+            function() vim.print "foo" end,
+            "Varible debug",
+          },
+        },
+      },
+    }, { prefix = "<leader>" })
+    require("debugprint").setup(opts)
+  end,
+  dependencies = {
+    "echasnovski/mini.nvim", -- Needed to enable :ToggleCommentDebugPrints for NeoVim <= 0.9
+    "nvim-treesitter/nvim-treesitter", -- Needed to enable treesitter for NeoVim 0.8
+  },
+  -- Remove the following line to use development versions,
+  -- not just the formal releases
+  version = "*",
+}
