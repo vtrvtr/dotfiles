ffc2cdefd2b6d9aa0ae65c91519ce2f3341418eb
Vitor Hugo <vitor.hugo@wildlifestudios.com>
2024-05-11 20:48:32 +1000

    Added grub replacer

---
+++ nvim/lua/plugins/grub-replace.lua
@@ -0,0 +1,15 @@
+-- lol
+return {
+  "MagicDuck/grug-far.nvim",
+  config = function()
+    require("grug-far").setup {
+      keymaps = {
+        replace = "<enter><enter>",
+        qflist = "<C-q>",
+        gotoLocation = "<enter>",
+        close = "<C-x>",
+      },
+    }
+    vim.cmd "cnoreabbrev Replace GrugFar"
+  end,
+}
