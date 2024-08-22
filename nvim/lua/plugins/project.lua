4d70a5e0ee056b2fee5de3294abf9788c614d56b
root <root@DESKTOP-VS3635G>
2024-08-21 22:34:07 -0700

    Changed detection mode for project nvim

--- nvim/lua/plugins/project.lua
+++ nvim/lua/plugins/project.lua
@@ -5,18 +5,17 @@ return {
       patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json", "Cargo.toml", "package.py" },
       manual_mode = true,
       silent_chdir = false,
+      detection_methods = { "pattern" },
     }
 
-    local wk = require "which-key";
+    local wk = require "which-key"
 
     wk.add {
-      {"<leader>fr",
-       function ()
-          require'telescope'.extensions.projects.projects{}
-       end ,
-       desc = "Recent projects"
-      }
+      {
+        "<leader>fr",
+        function() require("telescope").extensions.projects.projects {} end,
+        desc = "Recent projects",
+      },
     }
-
   end,
 }
