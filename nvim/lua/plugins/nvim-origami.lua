a7fb4365221607feafc8660d02311fc06243c339
root <root@DESKTOP-VS3635G>
2024-09-28 19:14:05 -0700

    Unused plugins removed

--- nvim/lua/plugins/nvim-origami.lua
+++
@@ -1,6 +0,0 @@
-return {
-  "chrisgrieser/nvim-origami",
-  enabled = false,
-  event = "BufReadPost", -- later or on keypress would prevent saving folds
-  opts = true, -- needed even when using default config
-}
