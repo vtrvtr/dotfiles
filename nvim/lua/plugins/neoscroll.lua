b0ddadd1450f42f62a4ee6565441c62cfbaa0dea
root <root@DESKTOP-VS3635G>
2024-09-28 19:14:36 -0700

    Updated deprecated setup

--- nvim/lua/plugins/neoscroll.lua
+++ nvim/lua/plugins/neoscroll.lua
@@ -6,11 +6,27 @@ return {
     require("neoscroll").setup {
       respect_scrolloff = true,
     }
-    local t = {}
+
+    local neoscroll = require "neoscroll"
     -- Syntax: t[keys] = {function, {function arguments}}
     -- Use the "sine" easing function
-    t["<C-k>"] = { "scroll", { "-vim.wo.scroll", "true", "350", [['quadratic']] } }
-    t["<C-j>"] = { "scroll", { "vim.wo.scroll", "true", "350", [['quadratic']] } }
-    require("neoscroll.config").set_mappings(t)
+    -- V
+    local keymap = {
+      ["<C-u>"] = function() neoscroll.ctrl_u { duration = 250, easing = "quadradic" } end,
+      ["<C-k>"] = function() neoscroll.ctrl_u { duration = 150, easing = "quadradic" } end,
+      ["<C-d>"] = function() neoscroll.ctrl_d { duration = 250, easing = "quadradic" } end,
+      ["<C-j>"] = function() neoscroll.ctrl_d { duration = 150, easing = "quadradic" } end,
+      ["<C-b>"] = function() neoscroll.ctrl_b { duration = 450, easing = "quadradic" } end,
+      ["<C-f>"] = function() neoscroll.ctrl_f { duration = 450, easing = "quadradic" } end,
+      ["<C-y>"] = function() neoscroll.scroll(-0.1, { move_cursor = false, duration = 100, easing = "quadradic" }) end,
+      ["<C-e>"] = function() neoscroll.scroll(0.1, { move_cursor = false, duration = 100, easing = "quadradic" }) end,
+      ["zt"] = function() neoscroll.zt { half_win_duration = 250, easing = "quadradic" } end,
+      ["zz"] = function() neoscroll.zz { half_win_duration = 250, easing = "quadradic" } end,
+      ["zb"] = function() neoscroll.zb { half_win_duration = 250, easing = "quadradic" } end,
+    }
+    local modes = { "n", "v", "x" }
+    for key, func in pairs(keymap) do
+      vim.keymap.set(modes, key, func)
+    end
   end,
 }
