69b71ae587b278aa71fcb1a52210dfffe1556758
root <root@DESKTOP-VS3635G>
2024-10-20 00:49:29 -0700

    Updated key for replace in grug

--- nvim/lua/plugins/grub-replace.lua
+++ nvim/lua/plugins/grub-replace.lua
@@ -1,16 +1,15 @@
--- lol
-return {
-  "MagicDuck/grug-far.nvim",
-  config = function()
-    require("grug-far").setup {
-      keymaps = {
-        replace = "<enter><enter>",
-        qflist = "<C-q>",
-        gotoLocation = "<enter>",
-        close = "<C-x>",
-      },
-    }
+return {
+  "MagicDuck/grug-far.nvim",
+  config = function()
+    require("grug-far").setup {
+      keymaps = {
+        replace = "<C-j>",
+        qflist = "<C-q>",
+        gotoLocation = "<enter>",
+        close = "<C-x>",
+      },
+    }
     vim.cmd "cnoreabbrev Replace GrugFar"
     vim.cmd "cnoreabbrev R GrugFar"
-  end,
-}
+  end,
+}
