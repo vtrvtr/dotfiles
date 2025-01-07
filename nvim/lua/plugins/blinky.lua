67165a240ad4efcce25bb47bbb0ac665febe9d2e
Vitor Hugo <vtr@pm.com>
2025-01-06 17:49:48 -0800

    Updated blink config

--- nvim/lua/plugins/blinky.lua
+++ nvim/lua/plugins/blinky.lua
@@ -15,19 +15,6 @@ return {
       preset = "default",
       ["<CR>"] = { "accept", "select_and_accept", "fallback" },
     },
-    windows = {
-      autocomplete = { border = "double" },
-      documentation = { border = "rounded" },
-    },
-    highlight = {
-      -- sets the fallback highlight groups to nvim-cmp's highlight groups
-      -- useful for when your theme doesn't support blink.cmp
-      -- will be removed in a future release, assuming themes add support
-      use_nvim_cmp_as_default = true,
-    },
-    -- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-    -- adjusts spacing to ensure icons are aligned
-    nerd_font_variant = "normal",
 
     completion = {
       documentation = { auto_show = true, window = { border = "double" } },
@@ -35,28 +22,21 @@ return {
       ghost_text = { enabled = true },
 
       menu = { draw = { treesitter = { "lsp " } }, border = "single" },
+      trigger = {
+        -- LSPs can indicate when to show the completion window via trigger characters
+        -- however, some LSPs (*cough* tsserver *cough*) return characters that would essentially
+        -- always show the window. We block these by default
+        show_on_blocked_trigger_characters = { " ", "\n", "\t", ":" },
+        -- when true, will show the completion window when the cursor comes after a trigger character when entering insert mode
+        show_on_insert_on_trigger_character = true,
+        -- list of additional trigger characters that won't trigger the completion window when the cursor comes after a trigger character when entering insert mode
+        show_on_x_blocked_trigger_characters = { "'", '"', "(" },
+      },
     },
 
     signature = {
       window = {
         border = "double",
-      },
-    },
-
-    trigger = {
-      completion = {
-        -- regex used to get the text when fuzzy matching
-        -- changing this may break some sources, so please report if you run into issues
-        -- todo: shouldnt this also affect the accept command? should this also be per language?
-        keyword_regex = "[%w_\\-]",
-        -- LSPs can indicate when to show the completion window via trigger characters
-        -- however, some LSPs (*cough* tsserver *cough*) return characters that would essentially
-        -- always show the window. We block these by default
-        blocked_trigger_characters = { " ", "\n", "\t", ":" },
-        -- when true, will show the completion window when the cursor comes after a trigger character when entering insert mode
-        show_on_insert_on_trigger_character = true,
-        -- list of additional trigger characters that won't trigger the completion window when the cursor comes after a trigger character when entering insert mode
-        show_on_insert_blocked_trigger_characters = { "'", '"' },
       },
     },
 
