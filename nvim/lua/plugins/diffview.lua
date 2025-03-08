d76b64992bea5249e68602fc75bb26f7743a4d8a
Vitor Hugo <vtr@pm.com>
2025-03-08 15:12:46 -0800

    feat(nvim): Added plugins for diffing

---
+++ nvim/lua/plugins/diffview.lua
@@ -0,0 +1,12 @@
+return {
+  "sindrets/diffview.nvim",
+
+  -- opts = {
+  --   -- your configuration comes here
+  --   -- or leave it empty to use the default settings
+  --   -- refer to the configuration section below
+  -- },
+  config = function()
+    require("diffview").setup {}
+  end,
+}
---
+++ nvim/lua/plugins/diffviewjj.lua
@@ -0,0 +1,10 @@
+return {
+  "rafikdraoui/jj-diffconflicts",
+
+  -- opts = {
+  --   -- your configuration comes here
+  --   -- or leave it empty to use the default settings
+  --   -- refer to the configuration section below
+  -- },
+  -- config = function() require("jj-diffconflicts").setup {} end,
+}
