5a3a517f7e68fc3a9a2a5e6239ace6b215a01385
Vitor Hugo <vtr@pm.com>
2025-03-23 13:02:58 -0700

    feat(nvim): astrov5

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -47,6 +47,34 @@ return {
     statuscolumn = { enabled = true },
     words = { enabled = true },
     animate = { enabled = true },
+    dashboard = {
+      -- your dashboard configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+      --
+      --
+      --
+      --
+      --
+      sections = {
+        { section = "keys", gap = 1, padding = 1 },
+        { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+        { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+        {
+          pane = 2,
+          icon = " ",
+          title = "Git Status",
+          section = "terminal",
+          enabled = function() return Snacks.git.get_root() ~= nil end,
+          cmd = "git status --short --branch --renames",
+          height = 5,
+          padding = 1,
+          ttl = 5 * 60,
+          indent = 3,
+        },
+        { section = "startup" },
+      },
+    },
   },
   keys = {
     { "<leader>,", function() Snacks.picker.buffers() end, desc = "Buffers" },
