f08f8adac19a605f0b9ef2479360e067176e55d6
Vitor Hugo <Vitor.Hugo@al.com.au>
2025-05-09 07:52:51 -0700

    feat(codecompantion/blink): llm stuff

---
+++ nvim/lua/plugins/codecompanion.lua
@@ -0,0 +1,60 @@
+return {
+  "olimorris/codecompanion.nvim",
+  opts = {
+    strategies = {
+      chat = {
+        adapter = "copilot",
+      },
+      inline = {
+        adapter = "copilot",
+      },
+    },
+    show_defaults = false,
+    adapters = {
+      copilot = function()
+        print(vim.inspect(require("codecompanion.adapters").extend("copilot").schema.model.choices()))
+        local adapters = require "codecompanion.adapters"
+        return adapters.extend("copilot", {
+          schema = {
+            model = {
+              default = "o3-mini",
+            },
+          },
+        })
+      end,
+    },
+  },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+    {
+      "zbirenbaum/copilot.lua",
+      cmd = "Copilot",
+      build = ":Copilot auth",
+      event = "BufReadPost",
+      dependencies = {},
+      opts = function(opts)
+        opts = {
+          suggestion = { enabled = false },
+          panel = { enabled = false },
+          filetypes = {
+            markdown = true,
+            help = true,
+          },
+        }
+        local wk = require "which-key"
+        wk.add {
+          mode = { "n" },
+          { "<leader>aa", "<cmd>CodeCompanionChat Toggle<CR>" },
+          desc = "Code companion Chat",
+        }
+        wk.add {
+          mode = { "n" },
+          { "<leader>ac", "<cmd>CodeCompanionActions<CR>" },
+          desc = "Code Companion actions",
+        }
+        return opts
+      end,
+    },
+  },
+}
---
+++ nvim/lua/plugins/min-pick.lua
@@ -0,0 +1,2 @@
+return { 'echasnovski/mini.pick', version = false }
+
