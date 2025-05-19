14ab53c8b4cf42ee04e8917157e301fc3390496a
Vitor Hugo <Vitor.Hugo@al.com.au>
2025-05-19 07:23:33 -0700

    chore(nvim): Updated kulala

--- nvim/lua/plugins/kuala.lua
+++ nvim/lua/plugins/kuala.lua
@@ -9,5 +9,28 @@ return {
   opts = {
     -- your configuration comes here
     global_keymaps = true,
+
+    contenttypes = {
+      ["application/vnd.pypi.simple.v1+json"] = {
+        ft = "json",
+        formatter = vim.fn.executable "jq" == 1 and { "jq", "." },
+        pathresolver = function(...) return require("kulala.parser.jsonpath").parse(...) end,
+      },
+      ["application/json"] = {
+        ft = "json",
+        formatter = vim.fn.executable "jq" == 1 and { "jq", "." },
+        pathresolver = function(...) return require("kulala.parser.jsonpath").parse(...) end,
+      },
+      ["application/xml"] = {
+        ft = "xml",
+        formatter = vim.fn.executable "xmllint" == 1 and { "xmllint", "--format", "-" },
+        pathresolver = vim.fn.executable "xmllint" == 1 and { "xmllint", "--xpath", "{{path}}", "-" },
+      },
+      ["text/html"] = {
+        ft = "html",
+        formatter = vim.fn.executable "xmllint" == 1 and { "xmllint", "--format", "--html", "-" },
+        pathresolver = nil,
+      },
+    },
   },
 }
