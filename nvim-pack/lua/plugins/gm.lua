7ca519b218879145577b3053ff1249d40de8076a
Vitor Hugo <Vitor.Hugo@al.com.au>
2026-09-10 09:44:01 -0700

    feat(nvim-pack): confirm gm mark replacement

--- nvim-pack/lua/plugins/gm.lua
+++ nvim-pack/lua/plugins/gm.lua
@@ -9,21 +9,30 @@ return {
 			auto_save = true, -- auto-save gm.txt on close
 		})
 
+		local store = require("gm.store")
 		local wk = require("which-key")
 
-		wk.add({
-			{
-				"m",
-				function()
-					gm.set_mark()
-					-- Setting a mark changes no buffer, so statusline listeners
-					-- have nothing else to key off.
-					vim.api.nvim_exec_autocmds("User", { pattern = "GmChanged" })
-				end,
-				desc = "Gm: Set mark",
-				mode = "n",
-			},
-		})
+		local function set_mark()
+			local key = vim.fn.getcharstr()
+			if key == "" or key == "<Esc>" or key == "\027" then
+				return
+			end
+
+			local mark, err = store.get(key)
+			if err then
+				vim.notify("gm: " .. err, vim.log.levels.ERROR)
+				return
+			end
+			if mark and vim.fn.confirm("Replace mark " .. key .. "?", "&Yes\n&No", 2) ~= 1 then
+				return
+			end
+
+			vim.api.nvim_feedkeys(key, "n", false)
+			gm.set_mark()
+			vim.api.nvim_exec_autocmds("User", { pattern = "GmChanged" })
+		end
+
+		vim.keymap.set("n", "m", set_mark, { desc = "Gm: Set mark" })
 		wk.add({
 			{
 				"mf",
