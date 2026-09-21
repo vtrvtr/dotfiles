114defe43d518862f314d6c199f1edf5c1c34e6f
Vitor Hugo <Vitor.Hugo@al.com.au>
2026-09-21 09:29:44 -0700

    feat: map git hunk navigation

--- nvim/lua/plugins/gitsigns.nvim.lua
+++ nvim/lua/plugins/gitsigns.nvim.lua
@@ -1,4 +1,23 @@
 -- Plugin: lewis6991/gitsigns.nvim
 -- Installed via store.nvim
 
-return { "lewis6991/gitsigns.nvim", event = "VeryLazy" }
+return {
+  "lewis6991/gitsigns.nvim",
+  event = "VeryLazy",
+  keys = {
+    {
+      "]g",
+      function()
+        require("gitsigns").next_hunk()
+      end,
+      desc = "Next git change",
+    },
+    {
+      "[g",
+      function()
+        require("gitsigns").prev_hunk()
+      end,
+      desc = "Previous git change",
+    },
+  },
+}
